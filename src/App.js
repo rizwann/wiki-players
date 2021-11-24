977c1780ad724bfa0bd653a172dff4e31c0248b4
rizwann <rizwankabirsizan@gmail.com>
2021-11-25 00:34:47 +0100

    ADD: Basic Search Component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,20 @@ import { useEffect, useState } from "react";
 import Cards from "./components/Cards/Cards";
 import Filter from './components/Filter/Filter'
 import Pagination from './components/Pagination/Pagination'
+import Search from './components/Search/Search'
+
 
 function App() {
 const url = 'https://rickandmortyapi.com/api'
 
 const [fetchedData, setFetchedData] = useState([]); 
 const [page, setPage] = useState(1)
+const [search, setSearch] = useState('')
+
 const {info, results} = fetchedData
 
 // console.log(results)
-let api = `${url}/character/?page=${page}`
+let api = `${url}/character/?page=${page}&name=${search}`
 
 
 
@@ -35,7 +39,7 @@ fetchData()
   return (
     <div className="App">
       <h1 className='text-center ubuntu my-4'>Rick and Morty <span className='text-primary'>WiKi</span> </h1>
-  
+  <Search setSearch={setSearch} setPage={setPage}/>
       <div className='container'>
         <div className='row'>
           <div className='col-3'>
---
+++ src/components/Search/Search.js
@@ -0,0 +1,25 @@
+import styles from "./Search.module.scss";
+
+const Search = ({ setSearch, setPage }) => {
+  return (
+    <form className="d-flex justify-content-center gap-4 mb-5">
+      <input
+        type="text"
+        className={styles.input}
+        placeholder="Search"
+        onChange={(e) => {
+          setPage(1);
+          setSearch(e.target.value);
+        }}
+      />
+      <button
+        className={`btn btn-primary fs-5 ${styles.btn}`}
+        onClick={(e) => e.preventDefault()}
+      >
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default Search;
