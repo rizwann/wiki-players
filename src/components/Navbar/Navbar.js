cb8df73b311d2a89c26f9768dc35a56d9cc5ab04
rizwann <rizwankabirsizan@gmail.com>
2021-11-30 18:47:04 +0100

    ADD: NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,40 +1,53 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
       <div className="container">
-        <Link to="/" className="fs-3 ubuntu navbar-brand">
-          Rick and Morty <span className="text-primary">WiKi</span>{" "}
+        <Link to="/" className="navbar-brand fs-3 ubuntu">
+          Rick and Morty <span className="text-primary">WiKi</span>
         </Link>
-      </div>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div
-        className="collapse navbar-collapse justify-content-end"
-        id="navbarNavAltMarkup"
-      >
-        <div className="navbar-nav fs-5">
-          <NavLink to="/" className="nav-link active" >
-           Characters
-          </NavLink>
-          <NavLink to="/episodes" className="nav-link">
-            Episodes
-          </NavLink>
-          <NavLink to="/location" className="nav-link">
-            Location
-          </NavLink>
+        <style jsx>{`
+          button[aria-expanded="false"] > .close {
+            display: none;
+          }
+          button[aria-expanded="true"] > .open {
+            display:none;
+          }
+        `}</style>
+        <button
+          className="navbar-toggler border-0"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="fas fa-bars open text-dark"></span>
+          <span class="fas fa-times close text-dark"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse justify-content-end"
+          id="navbarNavAltMarkup"
+        >
+          <div className="navbar-nav fs-5">
+            <NavLink to="/" className="nav-link">
+              Characters
+            </NavLink>
+            <NavLink to="/episodes" className="nav-link">
+              Episode
+            </NavLink>
+            <NavLink
+              activeClassName="active"
+              className="nav-link"
+              to="/location"
+            >
+              Location
+            </NavLink>
+          </div>
         </div>
       </div>
     </nav>
